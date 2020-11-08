bbea60a50d53c6f881a3034c0e8564c2e52ea3f9
Hassan Siddiqui <sidd_hassan@hotmail.com>
2020-11-08 22:18:48 +0500

    Initial Commit

--- App.js
+++ App.js
@@ -1,21 +1,66 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
-export default function App() {
+const App = () => {
+
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [goalsList, setGoalsList] = useState([]);
+
+  const goalInputHandler = enteredText => {
+    setEnteredGoal(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    setGoalsList([...goalsList,enteredGoal]);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+      <View 
+      style={styles.inputContainer}>
+         <TextInput 
+         placeholder="Mention Your Goal Here" 
+         style={styles.textInput} 
+         onChangeText={goalInputHandler} 
+         value={enteredGoal}
+         />
+         <Button 
+         title="+" 
+         style={styles.button}
+         onPress={addGoalHandler}
+         />
+      </View>
+      <View>
+        { goalsList.map(goal => <View key={goal} style={styles.listItem}><Text >{goal}</Text></View>)}
+      </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen:{
+    padding: 60
   },
+  inputContainer:{
+    flexDirection:'row', 
+    justifyContent: 'space-between', 
+    alignItems: 'center'
+  },
+  textInput:{ 
+    borderBottomColor: 'black', 
+    borderBottomWidth: 1, 
+    marginBottom: 5,
+    padding: 5,
+    width: '90%' 
+  },
+  listItem:{
+    marginVertical: 10,
+    padding: 10,
+    backgroundColor: 'lightgrey',
+    borderColor: 'black',
+    borderRadius: 5
+  }
 });
+
+export default App;
